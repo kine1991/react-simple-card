24a82bc802330afba0b74f77456939c6e0648a03
kine1991 <kine4599@gmail.com>
2019-07-20 12:21:00 +0300

    Updates

---
+++ precache-manifest.9d4758935916edcabd3222f52db2bcea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1c1ef20e767651a3d2571c25c49b5c",
+    "url": "/react-simple-card/index.html"
+  },
+  {
+    "revision": "2f89fd253d1700efe08e",
+    "url": "/react-simple-card/static/css/main.f71bcc69.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/react-simple-card/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "2f89fd253d1700efe08e",
+    "url": "/react-simple-card/static/js/main.2ff9a29e.chunk.js"
+  },
+  {
+    "revision": "22f6a79a0ee8e1a3d579",
+    "url": "/react-simple-card/static/js/runtime~main.00467590.js"
+  }
+]);
